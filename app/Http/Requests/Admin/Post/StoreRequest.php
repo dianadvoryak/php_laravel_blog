2af688f3b58a92610412e10e9c02b251b1f69b69
Diana Dvoryak <dianadvoryak.w@gmail.com>
2023-06-22 20:57:45 +0200

    changes

---
+++ app/Http/Requests/Admin/Post/StoreRequest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Requests\Admin\Post;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'title' => 'required|string',
+            'content' => 'required|string',
+            'preview_image' => 'required|file',
+            'main_image' => 'required|file',
+            'category_id' => 'required|integer|exists:categories,id',
+            'tag_ids' => 'nullable|array',
+            'tag_ids.*' => 'nullable|integer|exists:tags,id',
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'preview_image.file' => 'Select image please.',
+            'main_image.file' => 'Select image please.',
+            'tags_ids.array' => 'The tags field is required.',
+        ];
+    }
+}
---
+++ resources/views/admin/post/create.blade.php
@@ -0,0 +1,117 @@
+@extends('admin.layouts.main')
+@section('content')
+    <!-- Content Wrapper. Contains page content -->
+    <div class="content-wrapper">
+        <!-- Content Header (Page header) -->
+        <div class="content-header">
+            <div class="container-fluid">
+                <div class="row mb-2">
+                    <div class="col-sm-6">
+                        <h1 class="m-0">Add post</h1>
+                    </div><!-- /.col -->
+                    <div class="col-sm-6">
+                        <ol class="breadcrumb float-sm-right">
+                            <li class="breadcrumb-item"><a href="{{ route('admin.main.index') }}">Main</a></li>
+                            <li class="breadcrumb-item"><a href="{{ route('admin.post.index') }}">Posts</a></li>
+                            <li class="breadcrumb-item active">Add post</li>
+                        </ol>
+                    </div><!-- /.col -->
+                </div><!-- /.row -->
+            </div><!-- /.container-fluid -->
+        </div>
+        <!-- /.content-header -->
+
+        <!-- Main content -->
+        <section class="content">
+            <div class="container-fluid">
+                <!-- Small boxes (Stat box) -->
+                <div class="row">
+                    <div class="col-12">
+                        <form action="{{ route('admin.post.store') }}" method="POST" enctype="multipart/form-data">
+                            @csrf
+                            <div class="form-group" class="w-25">
+                                <input type="text" class="form-control" name="title" placeholder="Name of post"
+                                       value="{{ old('title') }}">
+                                @error('title')
+                                <div class="text-danger">{{$message}}</div>
+                                @enderror
+                            </div>
+                            <div class="form-group">
+                                <textarea id="summernote" name="content">{{ old('content') }}</textarea>
+                                @error('content')
+                                <div class="text-danger">{{$message}}</div>
+                                @enderror
+                            </div>
+                            <div class="form-group w-50">
+                                <label>Select category</label>
+                                <select name="category_id" class="form-control">
+                                    @foreach($categories as $category)
+                                        <option
+                                            value="{{$category->id}}" {{$category->id == old('category_id') ? 'selected' : ''}}>
+                                            {{$category->title}}
+                                        </option>
+                                    @endforeach
+                                </select>
+                                @error('category_id')
+                                <div class="text-danger">{{$message}}</div>
+                                @enderror
+                            </div>
+                            <div class="form-group">
+                                <label>Tags</label>
+                                <div class="select2-purple">
+                                    <select class="select2" name="tag_ids[]" multiple="multiple" data-placeholder="Select a tags"
+                                            data-dropdown-css-class="select2-purple" style="width: 100%;">
+                                        @foreach($tags as $tag)
+                                            <option {{ is_array( old('tag_ids')) && in_array($tag->id, old('tag_ids')) ? 'selected' : '' }} value="{{$tag->id}}">{{$tag->title}}</option>
+                                        @endforeach
+                                    </select>
+                                </div>
+                                @error('tag_ids')
+                                <div class="text-danger">{{$message}}</div>
+                                @enderror
+                            </div>
+
+                            <div class="form-group w-50">
+                                <label for="exampleInputFile">Add preview</label>
+                                <div class="input-group">
+                                    <div class="custom-file">
+                                        <input type="file" class="custom-file-input" name="preview_image">
+                                        <label class="custom-file-label">Choose preview image</label>
+                                    </div>
+                                    <div class="input-group-append">
+                                        <span class="input-group-text">Upload</span>
+                                    </div>
+                                </div>
+                                @error('preview_image')
+                                <div class="text-danger">{{$message}}</div>
+                                @enderror
+                            </div>
+                            <div class="form-group w-50">
+                                <label for="exampleInputFile">Add main image</label>
+                                <div class="input-group">
+                                    <div class="custom-file">
+                                        <input type="file" class="custom-file-input" name="main_image">
+                                        <label class="custom-file-label">Choose main image</label>
+                                    </div>
+                                    <div class="input-group-append">
+                                        <span class="input-group-text">Upload</span>
+                                    </div>
+                                </div>
+                                @error('main_image')
+                                <div class="text-danger">{{$message}}</div>
+                                @enderror
+                            </div>
+                            <div class="form-group">
+                                <input type="submit" class="btn btn-primary" value="Add">
+                            </div>
+                        </form>
+                    </div>
+                </div>
+                <!-- /.row -->
+
+            </div><!-- /.container-fluid -->
+        </section>
+        <!-- /.content -->
+    </div>
+    <!-- /.content-wrapper -->
+@endsection
